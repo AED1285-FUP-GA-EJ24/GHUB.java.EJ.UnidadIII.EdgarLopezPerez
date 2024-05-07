2207d7971b976db667325a3d60eddb2101c29c95
Edgar Jesus Lopez Perez <159453307+edgarlpz13@users.noreply.github.com>
2024-05-07 14:44:15 +0000

    promedio

---
+++ promedio/Principal.java
@@ -0,0 +1,11 @@
+package promedio;
+
+public class Principal { 
+    public static void main(String[] args) { 
+        Promedio objPromedio= new Promedio();
+        objPromedio.Detcalf();
+
+    }
+
+
+}
