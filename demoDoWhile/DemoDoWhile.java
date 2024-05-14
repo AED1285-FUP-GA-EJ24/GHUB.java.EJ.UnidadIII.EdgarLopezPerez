d9ae1fbec977903058a7ee7e96fdce1cd19f8bbe
Edgar Jesus Lopez Perez <159453307+edgarlpz13@users.noreply.github.com>
2024-05-14 15:04:36 +0000

    actualizacion

---
+++ demoDoWhile/DemoDoWhile.java
@@ -0,0 +1,21 @@
+package demoDoWhile;
+public class DemoDowhile { 
+    public void detDemoWhile() { 
+        System.out.println("Demostracion de un ciclo acendente utilizando do-while");
+        for(int i = 0; i <= 3; i++);{ 
+            
+        System.out.println("valor de i= "+i);
+        
+        }
+        int i;
+        System.out.println("Demostracion de un ciclo acendente utilizando do-while");
+        i=0;
+        do {
+            System.out.println("valor de i= "+i);
+            i++;
+            
+        } while (i<=3);
+
+
+    }
+}
